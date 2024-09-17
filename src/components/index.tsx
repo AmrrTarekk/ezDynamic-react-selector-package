ce465313d4e64ef2158be1f11aec5a0446e6adf9
AmrrTarekk <amrrtarek73@gmail.com>
2024-09-17 14:54:16 +0300

    edits

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -184,7 +184,7 @@ export const Selector = ({
                           xmlns="http://www.w3.org/2000/svg"
                           id="Layer_1"
                           data-name="Layer 1"
-                          viewBox="0 0 16 16"
+                          viewBox="0 0 24 24"
                           width="16"
                           height="16"
                         >
